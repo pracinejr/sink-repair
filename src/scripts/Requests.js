39c8800e840463fbb113bca0aea4fb3fe03e68c2
Peter Racine Jr <peterracinejr@gmail.com>
2021-06-16 15:41:26 -0500

    added delete request click event and fetch

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -1,4 +1,13 @@
-import { getRequests } from "./dataAccess.js";
+import { getRequests, deleteRequest } from "./dataAccess.js";
+
+const mainContainer = document.querySelector("#container");
+
+mainContainer.addEventListener("click", (click) => {
+  if (click.target.id.startsWith("request--")) {
+    const [, requestId] = click.target.id.split("--");
+    deleteRequest(parseInt(requestId));
+  }
+});
 
 export const Requests = () => {
   const requests = getRequests(); // grab the local state of the requests data
@@ -7,12 +16,13 @@ export const Requests = () => {
         <ul>
             ${requests
               .map((requestToList) => {
-                return `<li>${requestToList.description}</li>
-              `;
+                return `<li>${requestToList.description}<button class="request__delete"
+                id="request--${requestToList.id}">
+            Delete
+        </button></li>`;
               })
               .join("")}
-        </ul>
-    `;
+        </ul>`;
 
   return html;
 };
