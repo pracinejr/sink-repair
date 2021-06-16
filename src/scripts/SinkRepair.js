4e80c7a2e4b3c9faa22c07c62901961563a57d50
Peter Racine Jr <peterracinejr@gmail.com>
2021-06-16 09:23:05 -0500

    added fetch requests

---
+++ src/scripts/SinkRepair.js
@@ -0,0 +1,25 @@
+import { Requests } from "./Requests.js";
+
+document.addEventListener("click", (event) => {
+  const clickedItem = event.target;
+  if (clickedItem.id === "requestServiceButton") {
+    addRequestService();
+  }
+});
+
+export const SinkRepair = () => {
+  return `
+    <h1>Maude and Merle's Sink Repair</h1>
+    
+    <article class="
+    <section class="serviceForm">
+        <h2>Service Form</h2>
+        ${ServiceForm()}
+    </section>
+
+    <section class="serviceRequests">
+        <h2>Service Requests</h2>
+        ${Requests()}
+    </section>
+    `;
+};
---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+import { fetchRequests } from "./dataAccess.js";
+import { SinkRepair } from "./SinkRepair.js";
+
+const mainContainer = document.querySelector("#container");
+
+const render = () => {
+  fetchRequests().then(() => {
+    mainContainer.innerHTML = SinkRepair();
+  });
+};
+
+render();
